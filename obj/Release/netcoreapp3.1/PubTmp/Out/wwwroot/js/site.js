c4ab936410ee4b4d5e8701c94354a49fb72a8cfa
guimaia98 <comguilhermegui@hotmail.com>
2021-05-30 12:29:16 -0300

    BlackJack

---
+++ obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,28 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function calcularIMC() {
+    var formulario = document.getElementById("formulario");
+
+    var peso = formulario.peso.value;
+    var altura = formulario.altura.value;
+
+    var imc = peso / (altura * altura);
+
+    formulario.imc.value = imc;
+
+}
+
+function calcular_media() {
+    var notas = document.getElementById("notas");
+
+    var nota1 = parseFloat(document.getElementById("nota1").value);
+    var nota2 = parseFloat(document.getElementById("nota2").value);
+
+    var media = (nota1 + nota2) / 2;
+
+    notas.media.value = media;
+    
+}
